b676ecdd3b131bcadf86208a495a56705aa564a0
hathbanger <hathbanger.ah@gmail.com>
2015-05-03 16:45:41 -0700

    added some gallery style

--- gallery.php
+++ gallery.php
@@ -7,6 +7,7 @@
 		<meta name="author" content="Tor Hathaway"/>
                 <meta charset="UTF-8"/>
                 <link type="text/css" rel="stylesheet" href="css_stylesheet/style.css"/>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
 	</head>
 <body>
 	<div id="wrapper">
@@ -21,21 +22,21 @@
             <div id="content">
                 
                 
+                <div class="container">
                 
                 
-                
-                <img src="images/550551_560079037354780_139263020_n.jpg" alt=""/>
-                <img src="images/10714396_677633545677945_3472597815439630212_o (2).jpg" alt=""/>
-                <img src="images/1398744_10152007990373007_1084898482_o.jpg" alt=""/>
-                <img src="images/gmgc05WM.jpg" alt=""/>
-                <img src="images/gmgc12WM.jpg" alt=""/>
-                <img src="images/gmgc14WM.jpg" alt=""/>
-                <img src="images/handstand.jpg" alt=""/>
+                <img class="gallImg" src="images/550551_560079037354780_139263020_n.jpg" alt=""/>
+                <img class="gallImg" src="images/10714396_677633545677945_3472597815439630212_o (2).jpg" alt=""/>
+                <img class="gallImg" src="images/1398744_10152007990373007_1084898482_o.jpg" alt=""/>
+                <img class="gallImg" src="images/gmgc05WM.jpg" alt=""/>
+                <img class="gallImg" src="images/gmgc12WM.jpg" alt=""/>
+                <img class="gallImg" src="images/gmgc14WM.jpg" alt=""/>
+                <img class="gallImg" src="images/handstand.jpg" alt=""/>
                 
                 
             </div> <!-- End of Content Div -->
             
-            
+            </div>
             
             <div id="footer">
                 <?php include '_includes/footer.php'; ?> <!-- PHP to call footer -->
